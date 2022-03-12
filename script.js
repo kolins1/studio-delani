d282e94721ee2fbe0d68523d4a11e6a241e50a37
ckipkurui77@gmail.com <ckipkurui77@gmail.com>
2022-03-12 23:54:54 +0300

    added function to form-section

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
-$(document).ready(function(){
-    $("#designpic").click(function(){
-      $("#designpic").slideDown('2000').hide('1000');
-      $("#designtxt").show('2000');
+$(document).ready(function () {
+    $("#designpic").click(function () {
+        $("#designpic").slideDown('2000').hide('1000');
+        $("#designtxt").show('2000');
     });
-    $("#designtxt").click(function(){
-      $("#designtxt").slideUp('2000');
-      $("#designpic").slideDown('2000');
+    $("#designtxt").click(function () {
+        $("#designtxt").slideUp('2000');
+        $("#designpic").slideDown('2000');
     });
-  });
+});
 
-  $(document).ready(function(){
-    $("#developmentpic").click(function(){
-      $("#developmentpic").slideDown('2000').hide('1000');
-      $("#developmenttxt").show('2000');
+$(document).ready(function () {
+    $("#developmentpic").click(function () {
+        $("#developmentpic").slideDown('2000').hide('1000');
+        $("#developmenttxt").show('2000');
     });
-    $("#developmenttxt").click(function(){
-      $("#developmenttxt").slideUp('2000');
-      $("#developmentpic").slideDown('2000');
+    $("#developmenttxt").click(function () {
+        $("#developmenttxt").slideUp('2000');
+        $("#developmentpic").slideDown('2000');
     });
-  });
+});
 
-  $(document).ready(function(){
-    $("#productpic").click(function(){
-      $("#productpic").slideDown('2000').hide('1000');
-      $("#producttxt").show('2000');
+$(document).ready(function () {
+    $("#productpic").click(function () {
+        $("#productpic").slideDown('2000').hide('1000');
+        $("#producttxt").show('2000');
     });
-    $("#producttxt").click(function(){
-      $("#producttxt").slideUp('2000');
-      $("#productpic").slideDown('2000');
+    $("#producttxt").click(function () {
+        $("#producttxt").slideUp('2000');
+        $("#productpic").slideDown('2000');
     });
-  });
+});
+
+
+$(document).ready(function () {
+    $("form").submit(function (event) {
+        var name = $("input#NAME").val();
+        var email = $("input#EMAIL").val();
+        var message = $("textarea#MESSAGE").val();
+        if ($("input#NAME").val() && $("input#EMAIL").val()) {
+            alert(name + ", we have received your message. Thank you for reaching out to us.");
+        } else {
+            alert("Please enter your name and email!");
+        }
+
+    });
+
+ });
